ca333e5f4f2882a15a48a8589a21e26acd4587fd
Алексей Обухов <ilyaphill1998@gmail.com>
2024-07-22 15:35:50 +0300

    камеру вырубай

--- Program.cs
+++ Program.cs
@@ -4,27 +4,33 @@ using Microsoft.Extensions.DependencyInjection;
 using GunShop.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Настройка контекста базы данных
 builder.Services.AddDbContext<GunShopContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("GunShopContext") ?? throw new InvalidOperationException("Connection string 'GunShopContext' not found.")));
 
-// Add services to the container.
+// Добавление сервисов в контейнер
 builder.Services.AddControllersWithViews();
 
-
-
 var app = builder.Build();
+
+// Создание или обновление базы данных при запуске
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<GunShopContext>();
 
+    // Применение миграций для создания или обновления базы данных
+    context.Database.Migrate();
+
+    // Инициализация начальных данных
     SeedData.Initialize(services);
 }
 
-// Configure the HTTP request pipeline.
+// Конфигурация HTTP запросов
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
