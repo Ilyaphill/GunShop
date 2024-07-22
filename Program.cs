09252b5579a3d78ec27fae097d56657e08627800
Алексей Обухов <ilyaphill1998@gmail.com>
2024-07-22 12:17:42 +0300

    ку

--- Program.cs
+++ Program.cs
@@ -10,8 +10,7 @@ builder.Services.AddDbContext<GunShopContext>(options =>
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
