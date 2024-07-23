9b86a31428ee4227518ababb45049ac94c73fd27
Алексей Обухов <ilyaphill1998@gmail.com>
2024-07-23 15:03:01 +0300

    здоровенный яааазь

--- Program.cs
+++ Program.cs
@@ -43,6 +43,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Katalog}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
